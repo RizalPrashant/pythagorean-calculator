b0099f6a6bf655ec6fe66ecd7922c40d619370a5
Prashant Rizal <rizalprashant@Prashants-MacBook-Pro.local>
2021-07-03 21:31:12 -0700

    Toolbar added

--- app/src/main/java/com/prashantrizal/android/pythagoreancalculator/pythagorascalculator/CalculatorActivity.java
+++ app/src/main/java/com/prashantrizal/android/pythagoreancalculator/pythagorascalculator/CalculatorActivity.java
@@ -1,6 +1,7 @@
 package com.prashantrizal.android.pythagoreancalculator.pythagorascalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -41,6 +42,11 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
 
         user_input_value_a = (EditText) findViewById(R.id.user_input_value_a);
         user_input_value_b = (EditText) findViewById(R.id.user_input_value_b);
