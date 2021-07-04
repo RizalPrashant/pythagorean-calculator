0e40e2d5c7a7d7ea705814e5f38507dcdc089635
Prashant Rizal <rizalprashant@Prashants-MacBook-Pro.local>
2021-07-03 18:05:51 -0700

    MIgrated to Android 4.2.2

--- app/src/main/java/com/prashantrizal/android/pythagoreancalculator/pythagorascalculator/HelpActivity.java
+++ app/src/main/java/com/prashantrizal/android/pythagoreancalculator/pythagorascalculator/HelpActivity.java
@@ -2,11 +2,11 @@ package com.prashantrizal.android.pythagoreancalculator.pythagorascalculator;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.prashantrizal.android.pythagoreancalculator.MainActivity;
+import com.prashantrizal.android.pythagoreancalculator.pythagorascalculator.MainActivity;
 import com.prashantrizal.android.pythagoreancalculator.R;
 
 
