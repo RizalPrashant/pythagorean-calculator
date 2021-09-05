3834dcb5b7b01049fc8e1bfd26bb37bfd2542b70
Prashant Rizal <rizalprashant@Prashants-MacBook-Pro.local>
2021-09-04 20:39:37 -0700

    Fixed layout issues

---
+++ app/src/main/java/com/prashantrizal/android/categorypage/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.prashantrizal.android.categorypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.prashantrizal.android.pythagoreancalculator1.R;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final ListView list = findViewById(R.id.list);
+        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
+        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        list.setAdapter(customAdapter);
+    }
+}
---
+++ app/src/main/java/com/prashantrizal/android/categorypage/SubjectData.java
@@ -0,0 +1,12 @@
+package com.prashantrizal.android.categorypage;
+
+public class SubjectData {
+    String SubjectName;
+    String Link;
+    String Image;
+    public SubjectData(String subjectName, String link, String image) {
+        this.SubjectName = subjectName;
+        this.Link = link;
+        this.Image = image;
+    }
+}
